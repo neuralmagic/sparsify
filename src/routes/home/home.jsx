19a9f5ae588c0cca29f3be60782630d7b803ea7d
Kevin Escobar <kevin.rodriguez@neuralmagic.com>
2020-09-21 12:22:36 -0400

    Added generic page component. Impl iframe for home page, and GenericPage for 404 not found

--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -1,17 +1,47 @@
-import React from "react";
-import { Fab } from "@material-ui/core";
+import React, { useState } from "react";
+import { Box, Fab, CircularProgress } from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
 
+import { ReactComponent as NMLogo } from "./img/logo.svg";
+import GenericPage from "../../components/generic-page";
 import makeStyles from "./home-styles";
 import AbsoluteLayout from "../../components/absolute-layout";
 
 const useStyles = makeStyles();
+const HOME_URL = "https://www.neuralmagic.com";
 
 function Home() {
+  const [loadingStatus, setLoadingStatus] = useState("loading");
   const classes = useStyles();
+
+  fetch({
+    url: HOME_URL,
+    mode: 'nocors',
+  }).then(response => {
+    setLoadingStatus("success")
+  }).catch((error) => {
+    setLoadingStatus("error")
+  })
 
   return (
     <AbsoluteLayout>
+      {loadingStatus === "loading" && (
+        <Box width="100%" height="100%" display="flex" justifyContent="center" alignItems="center">
+          <CircularProgress/>
+        </Box>
+      )}
+      {loadingStatus === "success" && (
+        <iframe title={"Home"} width="100%" height="100%" src={HOME_URL} onError={error => console.log("failed")}>
+        </iframe>
+      )}
+      {loadingStatus === "error" && (
+        <GenericPage
+          title="Sparsify"
+          description="Select a project from the left or add a new project with the bottom right button to profile, benchmark, and optimize your Neural Network for production"
+          logoComponent={<NMLogo/>}
+        />
+      )}
+      
       <Fab
         variant="extended"
         color="secondary"
--- src/routes/not-found/not-found.jsx
+++ src/routes/not-found/not-found.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
-import AbsoluteLayout from "../../components/absolute-layout";
+import SentimentVeryDissatisfiedIcon from '@material-ui/icons/SentimentVeryDissatisfied';
+import GenericPage from "../../components/generic-page";
 
 function NotFound() {
-  return <AbsoluteLayout>Not Found</AbsoluteLayout>;
+  return <GenericPage
+    title="Page not found"
+    description="The page you are looking for doesn't exist or an other error occured."
+    logoComponent={<SentimentVeryDissatisfiedIcon/>}
+  />;
 }
 
 export default NotFound;

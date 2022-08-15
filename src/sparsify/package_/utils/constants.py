48ba68b4754f232d3cdd64b4b4540955774bbf48
Rahul Tuli <rahul@neuralmagic.com>
2022-08-15 03:15:07 -0400

    Adds the following:
    * Setup directory Structure
    * `from sparsify import package` importable + callable function
    * A constants file with supported tasks, criterions, and deployment scenarios (Should probably converted to `Enums` or something better than `python lists`)
    * Add `click` as a required dependency
    * Additional CLI helpers for updated acceptance criterion
    * `sparsify.package` cli utility
    * setup test directory
    * Add tests for CLI
    * Setup Entrypoints

---
+++ src/sparsify/package_/utils/constants.py
@@ -0,0 +1,58 @@
+# Copyright (c) 2021 - present / Neuralmagic, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+__all__ = [
+    "TASKS",
+    "METRICS",
+    "DEPLOYMENT_SCENARIOS",
+]
+
+TASKS = [
+    "ic",
+    "image-classification",
+    "image_classification",
+    "classification",
+    "od",
+    "object-detection",
+    "object_detection",
+    "detection",
+    "segmentation",
+    "qa",
+    "question-answering",
+    "question_answering",
+    "text-classification",
+    "text_classification",
+    "glue",
+    "sentiment",
+    "sentiment_analysis",
+    "sentiment-analysis",
+    "token-classification",
+    "token_classification",
+    "ner",
+    "named-entity-recognition",
+    "named_entity_recognition",
+]
+METRICS = [
+    "accuracy",
+    "f1",
+    "recall",
+    "mAP",
+    "latency",
+    "file_size",
+    "memory_usage",
+]
+
+DEPLOYMENT_SCENARIOS = [
+    "VNNI",
+]

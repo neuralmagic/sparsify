1d6eee94a914b5a80854cfd0e80a2ef1af327ba8
Rahul Tuli <rahul@neuralmagic.com>
2022-08-24 22:59:22 -0400

    Add dataset registry update cli to use dataset registry

--- src/sparsify/utils/constants.py
+++ src/sparsify/utils/constants.py
@@ -12,37 +12,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from collections import namedtuple
+
+
 __all__ = [
-    "TASKS",
     "METRICS",
     "DEPLOYMENT_SCENARIOS",
+    "TASKS",
+    "TASK_REGISTRY",
+    "TaskInfo",
+    "DATASET_REGISTRY",
+    "DATASETS",
 ]
 
-TASKS = [
-    "ic",
-    "image-classification",
-    "image_classification",
-    "classification",
-    "od",
-    "object-detection",
-    "object_detection",
-    "detection",
-    "segmentation",
-    "qa",
-    "question-answering",
-    "question_answering",
-    "text-classification",
-    "text_classification",
-    "glue",
-    "sentiment",
-    "sentiment_analysis",
-    "sentiment-analysis",
-    "token-classification",
-    "token_classification",
-    "ner",
-    "named-entity-recognition",
-    "named_entity_recognition",
-]
+TaskInfo = namedtuple("TaskInfo", "domain, subdomain")
+
 METRICS = [
     "accuracy",
     "f1",
@@ -50,10 +34,55 @@ METRICS = [
     "mAP",
     "compression",
     "latency",
-    "throughput",
+    "file_size",
+    "memory_usage",
 ]
 
 DEPLOYMENT_SCENARIOS = [
     "VNNI",
     "NO_VNNI",
 ]
+
+TASK_REGISTRY = {
+    "ic": TaskInfo(domain="cv", subdomain="classification"),
+    "image-classification": TaskInfo(domain="cv", subdomain="classification"),
+    "image_classification": TaskInfo(domain="cv", subdomain="classification"),
+    "classification": TaskInfo(domain="cv", subdomain="classification"),
+    "od": TaskInfo(domain="cv", subdomain="detection"),
+    "object-detection": TaskInfo(domain="cv", subdomain="detection"),
+    "object_detection": TaskInfo(domain="cv", subdomain="detection"),
+    "detection": TaskInfo(domain="cv", subdomain="detection"),
+    "segmentation": TaskInfo(domain="cv", subdomain="segmentation"),
+    "qa": TaskInfo(domain="nlp", subdomain="question_answering"),
+    "question-answering": TaskInfo(domain="nlp", subdomain="question_answering"),
+    "question_answering": TaskInfo(domain="nlp", subdomain="question_answering"),
+    "text-classification": TaskInfo(domain="nlp", subdomain="text_classification"),
+    "text_classification": TaskInfo(domain="nlp", subdomain="text_classification"),
+    "glue": TaskInfo(domain="nlp", subdomain="text_classification"),
+    "sentiment": TaskInfo(domain="nlp", subdomain="sentiment_analysis"),
+    "sentiment_analysis": TaskInfo(domain="nlp", subdomain="sentiment_analysis"),
+    "sentiment-analysis": TaskInfo(domain="nlp", subdomain="sentiment_analysis"),
+    "token-classification": TaskInfo(domain="nlp", subdomain="token_classification"),
+    "token_classification": TaskInfo(domain="nlp", subdomain="token_classification"),
+    "ner": TaskInfo(domain="nlp", subdomain="token_classification"),
+    "named-entity-recognition": TaskInfo(
+        domain="nlp", subdomain="token_classification"
+    ),
+    "named_entity_recognition": TaskInfo(
+        domain="nlp", subdomain="token_classification"
+    ),
+}
+
+DATASET_REGISTRY = {
+    "imagenette": TaskInfo(domain="cv", subdomain="classification"),
+    "imagenet": TaskInfo(domain="cv", subdomain="classification"),
+    "coco": TaskInfo(domain="cv", subdomain="detection"),
+    "squad": TaskInfo(domain="nlp", subdomain="question_answering"),
+    "mnli": TaskInfo(domain="nlp", subdomain="text_classification"),
+    "qqp": TaskInfo(domain="nlp", subdomain="text_classification"),
+    "sst2": TaskInfo(domain="nlp", subdomain="text_classification"),
+    "conll2003": TaskInfo(domain="nlp", subdomain="token_classification"),
+}
+
+TASKS = list(TASK_REGISTRY.keys())
+DATASETS = list(DATASET_REGISTRY.keys())

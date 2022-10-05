853e5b71a8a488908891c0f6ab90c004d6f8499c
Rahul Tuli <rahul@neuralmagic.com>
2022-10-05 10:51:38 -0400

    Add None to test

--- tests/sparsify/utils/test_constants.py
+++ tests/sparsify/utils/test_constants.py
@@ -11,21 +11,37 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from typing import Optional
 
 import pytest
 
 from sparsify.utils.constants import TaskName, get_dataset_info, get_task_info
 
 
-@pytest.mark.parametrize("task_name", ["ic", "image_Classification", "classification"])
-def test_get_task_info(task_name: str):
+@pytest.mark.parametrize(
+    "task_name",
+    [
+        "ic",
+        "image_Classification",
+        "classification",
+        None,
+        0,
+    ],
+)
+def test_get_task_info(task_name: Optional[str]):
     task_info = get_task_info(task_name)
-    assert task_info
-    assert isinstance(task_info, TaskName)
+    if task_name:
+        assert task_info
+        assert isinstance(task_info, TaskName)
+    else:
+        assert task_info is None
 
 
-@pytest.mark.parametrize("dataset_name", ["mnli", "MNLI", " mnLI "])
-def test_get_dataset_info(dataset_name: str):
+@pytest.mark.parametrize("dataset_name", ["mnli", "MNLI", " mnLI ", None, 0])
+def test_get_dataset_info(dataset_name: Optional[str]):
     dataset_info = get_dataset_info(dataset_name)
-    assert dataset_info
-    assert isinstance(dataset_info, TaskName)
+    if dataset_name:
+        assert dataset_info
+        assert isinstance(dataset_info, TaskName)
+    else:
+        assert dataset_info is None

84094062446f273a4a56c92970880e74fd5a4ace
rahul-tuli <rahul@neuralmagic.com>
2023-04-27 08:49:35 -0400

    * Add args to main over kwargs * Add a hidden debug option

--- src/sparsify/cli/init.py
+++ src/sparsify/cli/init.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import logging
 from typing import Optional
 
 import click
@@ -19,22 +20,40 @@ from sparsezoo.analyze.cli import CONTEXT_SETTINGS
 from sparsify.cli import opts
 
 
+_LOGGER = logging.getLogger(__name__)
+
+
 @click.command(context_settings=CONTEXT_SETTINGS)
 @opts.EXPERIMENT_TYPE
 @opts.add_info_opts
 @click.option("--model", required=True, help="Path to model")
 @opts.add_data_opts
+@click.option(
+    "--debug/--no-debug",
+    default=False,
+    hidden=True,
+)
 def main(
+    model: str,
     experiment_id: Optional[str],
     experiment_type: Optional[str],
     use_case: Optional[str],
-    **kwargs,
+    project_id: Optional[str],
+    working_dir: Optional[str],
+    data: Optional[str],
+    eval_metric: Optional[str],
+    train_samples: Optional[str],
+    val_samples: Optional[str],
+    debug: bool = False,  # hidden arg for debug logs
 ):
     """
     Sparsify.init CLI utility to initialize an experiment such that it will
     provision all local and cloud resources necessary, additionally also allows
     users to update hyper-param(s) before applying
     """
+    if debug:
+        logging.basicConfig(level=logging.DEBUG)
+
     if experiment_id is None:
         if experiment_type is None:
             raise ValueError(
@@ -44,6 +63,8 @@ def main(
             raise ValueError(
                 "--use-case required when --experiment-id is not specified."
             )
+
+    _LOGGER.debug(f"Local args: {locals()}")
 
 
 if __name__ == "__main__":

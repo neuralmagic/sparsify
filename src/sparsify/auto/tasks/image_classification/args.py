11aaa2e1d8817305aa726f2e879397f661c07269
Konstantin Gulin <66528950+KSGulin@users.noreply.github.com>
2023-06-01 19:58:06 +0100

    Update integration and unit tests (#214)
    * Update integration and unit tests
    
    * Update IC base test model

--- src/sparsify/auto/tasks/image_classification/args.py
+++ src/sparsify/auto/tasks/image_classification/args.py
@@ -57,11 +57,11 @@ class _ImageClassificationBaseArgs(BaseArgs):
     )
     pretrained: str = Field(default="True", description="type of pretrained weights")
     pretrained_dataset: str = Field(default=None, description="checkpoint data name")
-    model_kwargs: str = Field(
-        default=dict(), description="json string for model constructor args"
+    model_kwargs: Optional[Dict[str, Any]] = Field(
+        default_factory=dict, description="json string for model constructor args"
     )
-    dataset_kwargs: str = Field(
-        default=dict(), description="json string for dataset constructor args"
+    dataset_kwargs: Optional[Dict[str, Any]] = Field(
+        default_factory=dict, description="json string for dataset constructor args"
     )
     model_tag: str = Field(description="required - tag for model under save_dir")
     save_dir: Union[str, Path] = Field(default=ROOT)
